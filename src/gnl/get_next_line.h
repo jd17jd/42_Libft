f68e71888431753b8f2fb808daa847513991629b
Juan Diego Sanz Guevara <jvivas-g@c1r8s1.42madrid.com>
2024-09-13 16:20:04 +0200

    100% bien

--- src/gnl/get_next_line.h
+++ src/gnl/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jvivas-g <jvivas-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 16:18:24 by jvivas-g          #+#    #+#             */
-/*   Updated: 2024/09/13 04:57:48 by jvivas-g         ###   ########.fr       */
+/*   Updated: 2024/09/13 16:02:44 by jvivas-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+
+# include "../../inc/libft.h"
 
 /* BUFFER_SIZE = Bytes to read/write in each iteration of get_next_line() */
 
@@ -27,10 +29,4 @@ char	*ft_append(int fd, char *stash);
 char	*ft_move_start(char *stash);
 char	*get_next_line(int fd);
 
-/* AUX FUNCTIONS */
-void	ft_bzero(void *s, size_t n);
-void	*ft_calloc(size_t count, size_t size);
-size_t	ft_strlen(const char *s);
-char	*ft_strjoin(char *s1, char *s2);
-char	*ft_strchr(const char *s, int c);
 #endif
